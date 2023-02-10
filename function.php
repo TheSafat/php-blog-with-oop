bf17508863b0424ecaa9ab5293d56ce21185aa89
TheSafat <thesafat@gmail.com>
2023-02-11 00:23:33 +0600

    image file is working perfectly in database and folder. insert and delete operation is added.

---
+++ function.php
@@ -0,0 +1,36 @@
+<?php
+
+class CrudApp{
+    private $conn;
+    public function __construct(){
+        $this->conn = mysqli_connect('localhost', 'root', '', 'kader_db');
+    }
+
+    public function addData($data){
+        $name = $data['name'];
+        $roll = $data['roll'];
+        $img_name = $_FILES['img']['name'];
+        $tmp_name = $_FILES['img']['tmp_name'];
+
+        $sql = "INSERT INTO students (name, roll, img) VALUES ('$name', $roll, '$img_name')";
+
+        if(mysqli_query($this->conn, $sql)){
+            move_uploaded_file($tmp_name, 'upload/' . $img_name);
+            echo "data inserted successfully";
+        }
+    }
+
+    public function displayData(){
+        $sql = "SELECT * FROM students";
+        $result = mysqli_query($this->conn, $sql);
+        //$row = mysqli_fetch_assoc($result);
+        return $result;
+    }
+
+    public function deleteData($id){
+        $sql = "DELETE FROM students WHERE id=$id";
+        mysqli_query($this->conn, $sql);
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+    include('function.php');
+
+    $objCrudAdmin = new CrudApp();
+
+    if(isset($_POST['btn'])){
+        $objCrudAdmin->addData($_POST);
+    }
+
+    $rows = $objCrudAdmin->displayData();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        <table>
+            <tr>
+                <td>Name</td>
+                <td><input type="text" name="name" id=""></td>
+            </tr>
+            <tr>
+                <td>Roll</td>
+                <td><input type="text" name="roll" id=""></td>
+            </tr>
+            <tr>
+                <td>Image</td>
+                <td><input type="file" name="img" id=""></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="btn" id="" value="Submit"></td>
+            </tr>
+        </table>
+    </form>
+
+    <br><br><br>
+    <hr>
+
+    <table>
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Roll</td>
+            <td>Image</td>
+        </tr>
+        <?php while($row = mysqli_fetch_assoc($rows)){ ?>
+        <tr>
+            <td> <?php echo $row['id'] ?> </td>
+            <td> <?php echo $row['name'] ?> </td>
+            <td> <?php echo $row['roll'] ?> </td>
+            <td><img height="100px" src="upload/<?php echo $row['img'] ?>" alt=""></td>
+            <td><a href="edit.php?status=edit&&id=<?php echo $row['id'] ?>">Edit</a></td>
+            <td><a href="delete.php?status=delete&&id=<?php echo $row['id'] ?>">Delete</a></td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
